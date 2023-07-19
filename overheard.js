64d2f505ea7f61d89b2241d5baede2190cc66b8b
Flusion Jack <fusionboy504@gmail.com>
2023-07-19 10:32:17 +0300

    next update

--- overheard.js
+++ overheard.js
@@ -35,13 +35,24 @@
 const startStopBtn = document.getElementById("startStopBtn");
 const resetBtn = document.getElementById("resetBtn");
 
-// Variables for times
+// Variables for time value
 
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
 
-// Stop Watch Function
+// Variables for leading zero
+
+let leadingSeconds = 0;
+let leadingMinutes = 0;
+let leadingHours = 0;
+
+// Variables for set interval & timerstatus
+
+let timerInterval = null;
+let timerStatus = "stopped";
+
+// Stop Watch function
 
 function stopWatch() {
   seconds++;
@@ -56,8 +67,51 @@ function stopWatch() {
     }
   }
 
-  let displayTimer = (document.getElementById("timer").innerText =
-    hours + ":" + minutes + ":" + seconds);
+  if (seconds < 10) {
+    leadingSeconds = "0" + seconds.toString();
+  } else {
+    leadingSeconds = seconds;
+  }
+
+  if (minutes < 10) {
+    leadingMinutes = "0" + minutes.toString();
+  } else {
+    leadingMinutes = minutes;
+  }
+
+  if (hours < 10) {
+    leadingHours = "0" + hours.toString();
+  } else {
+    leadingHours = hours;
+  }
+
+  let displayTimer = (document.getElementById("timer").innerHTML =
+    leadingHours + ":" + leadingMinutes + ":" + leadingSeconds);
 }
 
-stopWatch();
+// window.setInterval(stopWatch, 1000);
+
+startStopBtn.addEventListener("click", function () {
+  if (timerStatus === "stopped") {
+    timerInterval = window.setInterval(stopWatch, 1000);
+    document.getElementById(
+      "startStopBtn"
+    ).innerHTML = `<i class='fa-solid fa-pause' id='pause'></i>`;
+    timerStatus = "started";
+  } else {
+    window.clearInterval(timerInterval);
+    document.getElementById(
+      "startStopBtn"
+    ).innerHTML = `<i class='fa-solid fa-play' id='play'></i>`;
+    timerStatus = "stopped";
+  }
+});
+
+resetBtn.addEventListener("click", function () {
+  window.clearInterval(timerInterval);
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+
+  document.getElementById("timer").innerHTML = "00:00:00";
+});
